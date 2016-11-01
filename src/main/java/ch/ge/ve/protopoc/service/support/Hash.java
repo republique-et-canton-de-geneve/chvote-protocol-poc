2bcc0c671c9dd9f14a967a98d01c4627e00dba0b
hoferth <hoferth@720a0dd1-f561-c264-a5d2-ea060933812d>
2016-11-01 08:57:20 +0000

    Add hashes
    git-svn-id: http://scm.etat-ge.ch:21080/sidp/sandbox/trunk/protocolPoc@21085 720a0dd1-f561-c264-a5d2-ea060933812d

--- src/main/java/ch/ge/ve/protopoc/service/support/Hash.java
+++ src/main/java/ch/ge/ve/protopoc/service/support/Hash.java
@@ -1,8 +1,81 @@
 package ch.ge.ve.protopoc.service.support;
 
+import ch.ge.ve.protopoc.service.model.SecurityParameters;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+
 /**
  * This class manages all the hashing operations and combinations
  */
 public class Hash {
+    private final String digestAlgorithm, digestProvider;
+    private final Conversion conversion;
 
+    public Hash(String digestAlgorithm, String digestProvider, SecurityParameters securityParameters, Conversion conversion) {
+        this.digestAlgorithm = digestAlgorithm;
+        this.digestProvider = digestProvider;
+        this.conversion = conversion;
+
+        try {
+            MessageDigest messageDigest = newMessageDigest();
+            if (messageDigest.getDigestLength()*8 != securityParameters.l) {
+                throw new IllegalArgumentException(
+                        String.format(
+                                "The length of the message digest should match the expected output length. " +
+                                        "Got %d expected %d",
+                                messageDigest.getDigestLength()*8,
+                                securityParameters.l));
+            }
+        } catch (NoSuchAlgorithmException | NoSuchProviderException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    private MessageDigest newMessageDigest() throws NoSuchAlgorithmException, NoSuchProviderException {
+        return MessageDigest.getInstance(digestAlgorithm, digestProvider);
+    }
+
+    public byte[] hash(Object... objects) throws NoSuchProviderException, NoSuchAlgorithmException {
+        MessageDigest messageDigest = newMessageDigest();
+        if (objects.length == 0) {
+            return messageDigest.digest();
+        } else if (objects.length == 1) {
+            return hash(objects[0]);
+        } else {
+            for (Object object : objects) {
+                messageDigest.update(hash(object));
+            }
+            return messageDigest.digest();
+        }
+    }
+
+    public byte[] hash(Object object) throws NoSuchProviderException, NoSuchAlgorithmException {
+        if (object instanceof String) {
+            return hash((String) object);
+        } else if (object instanceof BigInteger) {
+            return hash((BigInteger) object);
+        } else if (object instanceof byte[]) {
+            return hash((byte[]) object);
+        } else if (object instanceof Object[]) {
+            return hash((Object[]) object);
+        } else {
+            throw new IllegalArgumentException(String.format("Could not determine the type of object %s", object));
+        }
+    }
+
+    public byte[] hash(byte[] byteArray) throws NoSuchProviderException, NoSuchAlgorithmException {
+        MessageDigest messageDigest = newMessageDigest();
+        return messageDigest.digest(byteArray);
+    }
+
+    public byte[] hash(String s) throws NoSuchProviderException, NoSuchAlgorithmException {
+        return hash(conversion.toByteArray(s));
+    }
+
+    public byte[] hash(BigInteger integer) throws NoSuchProviderException, NoSuchAlgorithmException {
+        return hash(conversion.toByteArray(integer));
+    }
 }
---
+++ src/test/java/ch/ge/ve/protopoc/service/support/HashTest.groovy
@@ -0,0 +1,46 @@
+package ch.ge.ve.protopoc.service.support
+
+import ch.ge.ve.protopoc.service.model.SecurityParameters
+import spock.lang.Specification
+
+import javax.xml.bind.DatatypeConverter
+import java.nio.charset.Charset
+
+/**
+ * Missing javadoc!
+ */
+class HashTest extends Specification {
+    public static final Charset charset = Charset.forName("UTF-8")
+    def Hash hash
+    def SecurityParameters securityParameters
+    def Conversion conversion = Mock()
+
+    void setup() {
+        securityParameters = new SecurityParameters()
+        securityParameters.l = 512
+
+        hash = new Hash("SHA-512", "SUN", securityParameters, conversion)
+    }
+
+    def "hash(byte[])"() {
+        expect:
+        hash.hash(bytes) == digest
+
+        where:
+        bytes                    | digest
+        "test".getBytes(charset) | DatatypeConverter.parseHexBinary("ee26b0dd4af7e749aa1a8ee3c10ae9923f618980772e473f8819a5d4940e0db27ac185f8a0e1d5f84f88bc887fd67b143732c304cc5fa9ad8e6f57f50028a8ff")
+    }
+
+    def "hash(Object[])"() {
+        given:
+        conversion.toByteArray("test") >> "test".getBytes(charset)
+        conversion.toByteArray(_ as BigInteger) >> { args -> (args[0] as BigInteger).toByteArray() }
+
+        expect:
+        hash.hash(objects) == digest
+
+        where:
+        objects                                                               | digest
+        ["test", BigInteger.valueOf(42L), [0xCC, 0xFF] as byte[]] as Object[] | [63, -98, 73, -96, 58, 68, 104, -27, -28, -118, 71, -7, 76, -52, -121, 103, -33, -108, -4, 126, 106, 58, 15, -57, -39, 103, 111, 0, 38, 93, 29, 2, 115, -81, -84, -30, -121, -15, -95, -120, 84, 55, 100, 83, 80, 12, -113, -49, 69, 85, -92, 33, 85, -24, -5, -9, 93, -71, -30, -66, 116, -97, -84, 112] as byte[]
+    }
+}
