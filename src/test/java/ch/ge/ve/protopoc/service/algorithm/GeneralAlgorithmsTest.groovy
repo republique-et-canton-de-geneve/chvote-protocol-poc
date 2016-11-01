ff9394146f74f6b9539b6f27b3bb09bc391c3db4
hoferth <hoferth@720a0dd1-f561-c264-a5d2-ea060933812d>
2016-11-01 08:57:33 +0000

    Add tests for algorithms 5.5 and 5.6
    git-svn-id: http://scm.etat-ge.ch:21080/sidp/sandbox/trunk/protocolPoc@21089 720a0dd1-f561-c264-a5d2-ea060933812d

--- src/test/java/ch/ge/ve/protopoc/service/algorithm/GeneralAlgorithmsTest.groovy
+++ src/test/java/ch/ge/ve/protopoc/service/algorithm/GeneralAlgorithmsTest.groovy
@@ -13,6 +13,7 @@ import static ch.ge.ve.protopoc.service.support.BigIntegers.SEVEN
 import static ch.ge.ve.protopoc.service.support.BigIntegers.THREE
 import static ch.ge.ve.protopoc.service.support.BigIntegers.TWO
 import static java.math.BigInteger.ONE
+import static java.math.BigInteger.TEN
 
 /**
  * This specification defines the expected behaviour of the general algorithms
@@ -93,8 +94,39 @@ class GeneralAlgorithmsTest extends Specification {
         def generators = generalAlgorithms.getGenerators(3, eg)
 
         then:
-        4 * conversion.toInteger(_) >>> [BigIntegers.FIVE, BigIntegers.THREE, BigInteger.ONE, BigIntegers.SEVEN]
+        4 * conversion.toInteger(_) >>> [FIVE, THREE, ONE, SEVEN]
         4 * hash.hash(_ as Object[]) >> ([] as byte[])
         generators.containsAll(FIVE, THREE, SEVEN)
+    }
+
+    def "getNIZKPChallenge"() {
+        Object[] v, t;
+        v = new Object[0]
+        t = new Object[0]
+
+        when:
+        def challenge = generalAlgorithms.getNIZKPChallenge(v, t, ELEVEN)
+
+        then:
+        1 * hash.hash(v, t) >> ([0x0D] as byte[])
+        1 * conversion.toInteger([0x0D] as byte[]) >> BigInteger.valueOf(14)
+        challenge == THREE
+    }
+
+    def "getPublicChallenges"() {
+        Object[] v = new Object[0]
+
+        when:
+        def challenges = generalAlgorithms.getPublicChallenges(3, v, ELEVEN)
+
+        then:
+        1 * hash.hash(v, ONE) >> ([0x0A] as byte[])
+        1 * conversion.toInteger([0x0A] as byte[]) >> TEN
+        1 * hash.hash(v, TWO) >> ([0x03] as byte[])
+        1 * conversion.toInteger([0x03] as byte[]) >> THREE
+        1 * hash.hash(v, THREE) >> ([0x1F] as byte[])
+        1 * conversion.toInteger([0x1F] as byte[]) >> BigInteger.valueOf(31)
+
+        challenges.containsAll(TEN, THREE, BigInteger.valueOf(9L))
     }
 }
