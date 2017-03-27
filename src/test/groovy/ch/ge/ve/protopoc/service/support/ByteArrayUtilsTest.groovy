97a4a63816fc1b0962bef88e2e4423f44cbc8a58
Thomas Hofer <thomas.geek.hofer@gmail.com>
2017-03-27 17:07:17 +0200

    Fix byte marking... (wrong bit was getting set)

--- src/test/groovy/ch/ge/ve/protopoc/service/support/ByteArrayUtilsTest.groovy
+++ src/test/groovy/ch/ge/ve/protopoc/service/support/ByteArrayUtilsTest.groovy
@@ -14,9 +14,13 @@ class ByteArrayUtilsTest extends Specification {
         where:
         upper_b                  | m | m_max || result
         [0x00]                   | 0 | 3     || [0x00]
-        [0x00]                   | 1 | 3     || [0x01]
-        [0x00]                   | 2 | 3     || [0x10]
-        [0x00]                   | 3 | 3     || [0x11]
-        [0xFF, 0xFF, 0xFF, 0xFF] | 0 | 3     || [0xFE, 0xFF, 0xFE, 0xFF]
+        [0x00]                   | 1 | 3     || [0x80]
+        [0x00]                   | 2 | 3     || [0x08]
+        [0x00]                   | 3 | 3     || [0x88]
+        [0xFF, 0xFF, 0xFF, 0xFF] | 0 | 3     || [0x7F, 0xFF, 0x7F, 0xFF]
+        [0xC1, 0xD2]             | 0 | 3     || [0x41, 0x52]
+        [0xC1, 0xD2]             | 1 | 3     || [0xC1, 0x52]
+        [0xC1, 0xD2]             | 0 | 15    || [0x41, 0x52]
+        [0xCC, 0xDD]             | 0 | 15    || [0x44, 0x55]
     }
 }
